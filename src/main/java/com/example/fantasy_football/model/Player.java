fc6539add0e579838209b52539d96c133fe490ec
Soap-jpg <mason.alex2102@gmail.com>
2026-03-31 02:57:07 +0530

    added validation to Player model fields

--- src/main/java/com/example/fantasy_football/model/Player.java
+++ src/main/java/com/example/fantasy_football/model/Player.java
@@ -1,9 +1,7 @@
 package com.example.fantasy_football.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,11 +17,29 @@ public class Player {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long playerId;
+
+    @NotBlank(message = "First name can't be empty")
+    @Size(min=2, max = 50, message = "Name must be between 2 and 50 characters")
     private String firstName;
+
+    @NotBlank(message = "Last name can't be empty")
+    @Size(min=2, max = 50, message = "Name must be between 2 and 50 characters")
     private String lastName;
+
+    @NotBlank(message = "Club name can't be empty")
     private String club;
+
+    @NotBlank(message = "Nationality can't be empty")
     private String nationality;
+
+    @NotNull(message = "Position is required")
+    @Enumerated(EnumType.STRING)
     private String position;
+
+    @Min(value=1,message="Jersey number must be at least 1")
+    @Max(value = 99, message = "Jersey number can't exceed 99")
     private Integer jerseyNumber;
+
+    @DecimalMin(value = "0.0",inclusive = true,message = "Market value can't be negative")
     private BigDecimal marketValue;
 }
