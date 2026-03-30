f34221be1f5a040e2835d9ba8b2c43589bd5e0b3
Soap-jpg <mason.alex2102@gmail.com>
2026-03-31 02:56:37 +0530

    added a repository function existsByClubAndJerseyNumber for service implementation of it

--- src/main/java/com/example/fantasy_football/repositories/PlayerRepository.java
+++ src/main/java/com/example/fantasy_football/repositories/PlayerRepository.java
@@ -4,4 +4,5 @@ import com.example.fantasy_football.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+    boolean existsByClubAndJerseyNumber(String club, Integer jerseyNumber);
 }
